eaaea805e4143c19ed6f82580959a9136d972747
besrour walid <walidbesrour@gmail.com>
2021-04-07 10:49:13 +0100

    talan app

--- app/src/main/java/com/example/talan_app/services/Service_Detail.kt
+++ app/src/main/java/com/example/talan_app/services/Service_Detail.kt
@@ -3,10 +3,44 @@ package com.example.talan_app.services
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.talan_app.R
+import com.example.talan_app.adapters.ViewAdapterPageView
+import com.example.talan_app.databinding.ActivityServiceDetailBinding
+import com.example.talan_app.detail.detail_actif.CompteurFragment
+import com.example.talan_app.detail.detail_actif.FilsDetailFragment
+import com.example.talan_app.detail.detail_actif.RisquePrecautionFragment
+import com.example.talan_app.services.detail_service.DateFragment
+import com.example.talan_app.services.detail_service.EtatActifFragment
+import com.example.talan_app.services.detail_service.JournalFragment
+import com.example.talan_app.services.detail_service.UtilisateurFragment
 
 class Service_Detail : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_service__detail)
+
+    private lateinit var binding: ActivityServiceDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState)
+        binding = ActivityServiceDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpTabs()
+
+
+    }
+
+
+    private fun setUpTabs() {
+        val adapter = ViewAdapterPageView(supportFragmentManager)
+        adapter.addFragment(DateFragment(), "Date")
+        adapter.addFragment(UtilisateurFragment(), "Utilisateur")
+        adapter.addFragment(EtatActifFragment(), "Etat de l'actif")
+        adapter.addFragment(JournalFragment(), "Journal des travaux")
+
+        binding.viewPageService.adapter = adapter
+        binding.tabDetailService.setupWithViewPager(binding.viewPageService)
+        binding.tabDetailService.getTabAt(0)!!
+        binding.tabDetailService.getTabAt(1)!!
+        binding.tabDetailService.getTabAt(2)!!
+        binding.tabDetailService.getTabAt(3)!!
+
+
     }
 }
---
+++ app/src/main/java/com/example/talan_app/services/detail_service/EtatActifFragment.kt
@@ -0,0 +1,20 @@
+package com.example.talan_app.services.detail_service
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.talan_app.R
+
+
+class EtatActifFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_etat_actif, container, false)
+    }
+
+
+}
