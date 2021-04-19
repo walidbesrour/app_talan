57647eb52a2794ee6e532af6cdbdb916d5d7864f
besrour walid <walidbesrour@gmail.com>
2021-04-19 14:11:03 +0100

    Talan app Realisation

---
+++ app/src/main/java/com/example/talan_app/intervention/detail_intervention/detail_plan/ServicesPlanFragment.kt
@@ -0,0 +1,19 @@
+package com.example.talan_app.intervention.detail_intervention.detail_plan
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.talan_app.R
+
+class ServicesPlanFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+     
+        return inflater.inflate(R.layout.fragment_services_plan, container, false)
+    }
+
+
+}
