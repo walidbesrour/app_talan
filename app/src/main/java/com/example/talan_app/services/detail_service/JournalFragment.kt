4b9644d4b1b3e38ae1111a38eac8749c579cb486
besrour walid <walidbesrour@gmail.com>
2021-04-25 23:41:32 +0100

    Talan app

--- app/src/main/java/com/example/talan_app/services/detail_service/JournalFragment.kt
+++ app/src/main/java/com/example/talan_app/services/detail_service/JournalFragment.kt
@@ -1,15 +1,24 @@
 package com.example.talan_app.services.detail_service
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.talan_app.R
 import com.example.talan_app.adapters.Adapter_List_Journal
 import com.example.talan_app.databinding.FragmentJournalBinding
 import com.example.talan_app.view_model.JournalVM
+import com.google.android.material.datepicker.MaterialDatePicker
 
 
 class JournalFragment : Fragment() {
@@ -30,9 +39,53 @@ class JournalFragment : Fragment() {
             binding.recycleJournal.adapter = adapter_List_Journal
         })
 
+        binding.addjournal.setOnClickListener {
+            viewOk()
+        }
 
         return binding.root
     }
+    fun viewOk(){
+        val view = View.inflate(requireActivity(), R.layout.dialog_journal,null)
+        val builder = AlertDialog.Builder(requireActivity())
+        builder.setView(view)
 
+        val dialog = builder.create()
+        dialog.show()
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        dialog.setCancelable(false)
+
+        dialog.findViewById<Button>(R.id.btnok1)?.setOnClickListener { dialog.dismiss() }
+
+        var txt : EditText =  dialog.findViewById<EditText>(R.id.DateJoural) as EditText
+
+        dialog.findViewById<EditText>(R.id.DateJoural)?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("Date", start.toString())
+                println("*********** $count")
+                if (count == 0){
+                    val builder : MaterialDatePicker.Builder<*> = MaterialDatePicker.Builder.datePicker()
+                    builder.setTitleText("selectionner la Date")
+                    val picker : MaterialDatePicker<*> = builder.build()
+                    picker.show(childFragmentManager , picker.toString())
+                    picker.addOnPositiveButtonClickListener {
+                        txt.setText(picker.headerText)
+
+                    }
+                }
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+
+    }
 
 }
